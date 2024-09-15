8a1492b50ba1203c25db92d48ac3cb213d63d2e9
SiddhantKumarMaurya <siddhantkumarmaurya@gmail.com>
2024-09-15 16:13:31 +0530

    Updated routing to use Routes and Route for react-router-dom v6

--- mern-friend-app/client/src/App.js
+++ mern-friend-app/client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
@@ -14,11 +14,11 @@ function App() {
                     <Link to="/home">Home</Link>
                 </nav>
 
-                <Switch>
-                    <Route path="/register" component={Register} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/home" component={Home} />
-                </Switch>
+                <Routes>
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
             </div>
         </Router>
     );
