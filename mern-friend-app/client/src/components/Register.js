be512c7aebd26723e8312643f243f921885aecfa
SiddhantKumarMaurya <siddhantkumarmaurya@gmail.com>
2024-09-17 22:59:37 +0530

    Made Register.js more readable

--- mern-friend-app/client/src/components/Register.js
+++ mern-friend-app/client/src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // To redirect after registration
-import "./style.css"; // Assuming your own custom styles
+import "./style.css";
 
 const Register = () => {
     const [formData, setFormData] = useState({ username: '', password: '', interests: '' });
@@ -15,6 +15,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
+            // To store the interests and hobbies
             const interestsArray = formData.interests.split(',').map(interest => interest.trim());
             const res = await axios.post('http://localhost:5000/api/auth/register', {
                 username: formData.username,
@@ -22,7 +23,9 @@ const Register = () => {
                 interests: interestsArray,
             });
             setMessage(res.data?.msg || 'Registration successful!');
-            navigate('/login'); // Redirect to login page after successful registration
+
+             // Redirect to login page after successful registration
+            navigate('/login');
         } catch (err) {
             if (err.response && err.response.data) {
                 setMessage(err.response.data.msg);
@@ -33,22 +36,22 @@ const Register = () => {
     };
 
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8 transition-colors duration-300">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">Create a New Account</h2>
-                <p className="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
+        <div className="register-container">
+            <div className="register-container-heading">
+                <h2>Create a New Account</h2>
+                <p>
                     Fill in the details below to create a new account.
                 </p>
             </div>
 
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div className="bg-white dark:bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="outer-container">
+                <div className="form-outer-container">
+                    <form onSubmit={handleSubmit}>
                         <div>
-                            <label htmlFor="username" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                            <label htmlFor="username">
                                 Username
                             </label>
-                            <div className="mt-1">
+                            <div className="input-container">
                                 <input
                                     id="username"
                                     name="username"
@@ -56,16 +59,15 @@ const Register = () => {
                                     required
                                     value={formData.username}
                                     onChange={handleChange}
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm placeholder-gray-400 dark:placeholder-gray-500 text-gray-900 dark:text-gray-100 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50 dark:bg-gray-700"
                                 />
                             </div>
                         </div>
 
                         <div>
-                            <label htmlFor="password" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                            <label htmlFor="password">
                                 Password
                             </label>
-                            <div className="mt-1">
+                            <div className="input-container">
                                 <input
                                     id="password"
                                     name="password"
@@ -73,16 +75,15 @@ const Register = () => {
                                     required
                                     value={formData.password}
                                     onChange={handleChange}
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm placeholder-gray-400 dark:placeholder-gray-500 text-gray-900 dark:text-gray-100 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50 dark:bg-gray-700"
                                 />
                             </div>
                         </div>
 
                         <div>
-                            <label htmlFor="interests" className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                            <label htmlFor="interests">
                                 Hobbies/Interests
                             </label>
-                            <div className="mt-1">
+                            <div className="input-container">
                                 <input
                                     id="interests"
                                     name="interests"
@@ -91,23 +92,19 @@ const Register = () => {
                                     required
                                     value={formData.interests}
                                     onChange={handleChange}
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm placeholder-gray-400 dark:placeholder-gray-500 text-gray-900 dark:text-gray-100 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-50 dark:bg-gray-700"
                                 />
                             </div>
                         </div>
 
                         <div>
-                            <button
-                                type="submit"
-                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
+                            <button type="submit">
                                 Sign Up
                             </button>
                         </div>
                     </form>
 
                     {message && (
-                        <div className="mt-4 text-center text-sm text-gray-600 dark:text-gray-400">
+                        <div className="message">
                             {message}
                         </div>
                     )}
